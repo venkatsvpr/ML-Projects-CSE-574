correcting the formula

2_HandWritten_Digit_Classification/basecode/nnScript.py
@@ -122,9 +122,11 @@ def preprocess():
 
     features_to_delete = []
     Number_of_Features = np.shape(validation_data)[1]
+
     # we have to ignore features that are not of importance to us.  
     # we use numpy.ptp
     # https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ptp.html
+    
     for i in range(Number_of_Features):
         # If feature is of no importance in training data
         if np.ptp(train_data[:,i]) == 0:
@@ -207,26 +209,36 @@ def nnObjFunction(params, *args):
 
     # Find the error and then use the formula to find the Gradient and value.
     Delta = FinalOutput - training_label
+    
+    # Using the formula  shared in handout. 
     Gradient_w2 = np.dot(Delta.T,HiddenOutput)
     Gradient_w1 = np.dot(((1-HiddenOutput)*HiddenOutput* (np.dot(Delta,w2))).T,training_data)
 
     # remove zero rows hidden
+    # np.delete(Gradient_w1 )
     Gradient_w1 = np.delete(Gradient_w1, n_hidden,0)
 
     # Implementing the formula from the document
-    o_part_1 = (np.sum(-1*(training_label*np.log(FinalOutput)+(1-training_label)*np.log(1-FinalOutput))))/num_samples
-    o_part_2 = (lambdaval/(2*num_samples))* ( np.sum(np.square(w1)) + np.sum(np.square(w2)))
-
-    # obj_grad 
-    obj_grad = np.array([])
+    # Find the sum of elements on the axis
+    logFinal = np.log(FinalOutput)
+    logOneFinal  = np.log(1-FinalOutput)
+    o_part_1 = (np.sum(-1*(training_label*logFinal+(1-training_label)*logOneFinal)))
+    o_part_1 = o_part_1/num_samples
+    sw1 = np.sum(np.square(w1))
+    sw2 = np.sum(np.square(w2))
+    o_part_2 = (lambdaval/(2*num_samples))* (sw1 +  sw2)
+    obj_val = o_part_1 + o_part_2
 
     # concatenate
+    # regularization will not impact for lambdaval 0, for others it will
     Gradient_w1 = Gradient_w1 + lambdaval * w1
     Gradient_w2 = Gradient_w2 + lambdaval * w2
+
+    # obj_grad 
+    obj_grad = np.array([])  
     obj_grad = np.concatenate((Gradient_w1.flatten(), Gradient_w2.flatten()),0)
     obj_grad = obj_grad/num_samples
     
-    obj_val = o_part_1 + o_part_2
     return (obj_val,obj_grad)
 
 def nnPredict(w1, w2, data):