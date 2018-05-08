00426d8cf3560027dc44984c97ace7d5f8ab78dc
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2018-05-07 22:01:47 -0400

    changes

--- 3_Multiclass_Logistic_Regression/basecode/script.py
+++ 3_Multiclass_Logistic_Regression/basecode/script.py
@@ -1,25 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from scipy.optimize import minimize
-
 from sklearn.svm import SVC
 
 def preprocess():
-    """ 
+    """
      Input:
      Although this function doesn't have any input, you are required to load
      the MNIST data set from file 'mnist_all.mat'.
 
      Output:
-     train_data: matrix of training set. Each row of train_data contains 
+     train_data: matrix of training set. Each row of train_data contains
        feature vector of a image
      train_label: vector of label corresponding to each image in the training
        set
-     validation_data: matrix of training set. Each row of validation_data 
+     validation_data: matrix of training set. Each row of validation_data
        contains feature vector of a image
-     validation_label: vector of label corresponding to each image in the 
+     validation_label: vector of label corresponding to each image in the
        training set
-     test_data: matrix of training set. Each row of test_data contains 
+     test_data: matrix of training set. Each row of test_data contains
        feature vector of a image
      test_label: vector of label corresponding to each image in the testing
        set
@@ -90,45 +90,31 @@ def sigmoid(z):
 
 
 def blrObjFunction(initialWeights, *args):
-    """
-    blrObjFunction computes 2-class Logistic Regression error function and
-    its gradient.
-
-    Input:
-        initialWeights: the weight vector (w_k) of size (D + 1) x 1 
-        train_data: the data matrix of size N x D
-        labeli: the label vector (y_k) of size N x 1 where each entry can be either 0 or 1 representing the label of corresponding feature vector
-
-    Output: 
-        error: the scalar value of error function of 2-class logistic regression
-        error_grad: the vector of size (D+1) x 1 representing the gradient of
-                    error function
-    """
     train_data, labeli = args
+    #print ("num samples  and feature -- ",train_data.shape[0],train_data.shape[1])
     n_data = train_data.shape[0]
     n_features = train_data.shape[1]
-
+    # Adding bias term
     BiasTerm = np.ones((train_data.shape[0],1))
     InputWithBias = np.column_stack((BiasTerm, train_data))
-
-    Theta = np.zeros((InputWithBias.shape[0],1))
-    
+    # print (" shape ",InputWithBias.shape[0],InputWithBias.shape[1])
     W = initialWeights.reshape(InputWithBias.shape[1],1)
-    
+    Theta = np.zeros((InputWithBias.shape[0],1))
     Theta = sigmoid(np.dot(InputWithBias,W))
+    # print (" thetaa shape ",Theta.shape[0],Theta.shape[1])
     LogTheta = np.log(Theta)
- 
     y=np.dot(labeli.transpose(),LogTheta)
- 
+
+    # implement the formula for error
     part1 =  np.dot(labeli.transpose(), LogTheta)
     part2 = np.dot(np.subtract(1.0,labeli).transpose(), np.log(np.subtract(1.0,Theta)))
     error = np.sum(part1 + part2)
     error = (-error)/InputWithBias.shape[0]
-   
-    error_grad = np.zeros((InputWithBias.shape[0], 1))       
+
+    # Implement the formula for error_grad
+    error_grad = np.zeros((InputWithBias.shape[0], 1))
     part3 = np.zeros((InputWithBias.shape[0],1))
     part3 = np.subtract(Theta,labeli)
-
     error_grad = np.dot(InputWithBias.transpose(), part3)
     error_grad=error_grad/InputWithBias.shape[0]
 
@@ -137,84 +123,62 @@ def blrObjFunction(initialWeights, *args):
 
 
 def blrPredict(W, data):
-    """
-     blrObjFunction predicts the label of data given the data and parameter W 
-     of Logistic Regression
-     
-     Input:
-         W: the matrix of weight of size (D + 1) x 10. Each column is the weight 
-         vector of a Logistic Regression classifier.
-         X: the data matrix of size N x D
-         
-     Output: 
-         label: vector of size N x 1 representing the predicted label of 
-         corresponding feature vector given in data matrix
-
-    """
-    label = np.zeros((data.shape[0], 1))
-    BiasTerm = np.ones((data.shape[0],1))
+    Num_of_Samples = data.shape[0]
+    label = np.zeros((Num_of_Samples, 1))
+    BiasTerm = np.ones((Num_of_Samples,1))
     InputWithBias = np.column_stack((BiasTerm, data))
-
+    # print ("shape is ",InputWithBias.shape[0],InputWithBias.shape[1])
     All_Labels = np.zeros((InputWithBias.shape[0],10))
     All_Labels = sigmoid(np.dot(InputWithBias,W))
-
+    # use argmax and find the lable with the max value
     label = np.argmax(All_Labels, axis =1)
-    label = label.reshape(data.shape[0],1)
+    # print (" shape of label is ",label.shape[0],label.shape[1])
+    # reshpae and return the label
+    label = label.reshape(Num_of_Samples,1)
     return label
 
 def mlrObjFunction(params, *args):
-    """
-    mlrObjFunction computes multi-class Logistic Regression error function and
-    its gradient.
+    train_data, labels = args
+    #print (" n ",train_data.shape[0],"features",train_data.shape[0],"labels",labels.shape[1])
+    Num_of_Features = train_data.shape[1]
+    Num_of_Samples = train_data.shape[0]
+    Num_of_Labels = labels.shape[1]
 
-    Input:
-        initialWeights: the weight vector of size (D + 1) x 1
-        train_data: the data matrix of size N x D
-        labeli: the label vector of size N x 1 where each entry can be either 0 or 1
-                representing the label of corresponding feature vector
+    # Add the bias term
+    BiasTerm = np.ones((Num_of_Samples,1))
+    InputWithBias = np.column_stack((BiasTerm, train_data))
+    # reshape the weights and find exp
+    Weights = np.reshape(params,(Num_of_Features + 1, Num_of_Labels))
+    exp_WTx = np.exp(np.dot(InputWithBias, Weights))
+    weight_sum = np.sum(exp_WTx,axis=1)
+    Thetank  = np.transpose(np.divide(np.transpose(exp_WTx), weight_sum))
+    # print (" size of thetank ",Thetank.shape[0],Thetank.shape[1])
 
-    Output:
-        error: the scalar value of error function of multi-class logistic regression
-        error_grad: the vector of size (D+1) x 10 representing the gradient of
-                    error function
-    """
-    n_data = train_data.shape[0]
-    n_feature = train_data.shape[1]
+    # Find the error
     error = 0
-    error_grad = np.zeros((n_feature + 1, n_class))
-
-    ##################
-    # YOUR CODE HERE #
-    ##################
-    # HINT: Do not forget to add the bias term to your input data
-
+    error = np.multiply(labels, np.log(Thetank))
+    error = -1 * np.sum(error)/Num_of_Samples
+    # Find the error gradient
+    error_grad = np.zeros((Num_of_Features + 1, 10))
+    error_grad = np.dot(np.transpose(InputWithBias), (Thetank - labels)).flatten()/Num_of_Samples
     return error, error_grad
 
-
 def mlrPredict(W, data):
-    """
-     mlrObjFunction predicts the label of data given the data and parameter W
-     of Logistic Regression
-
-     Input:
-         W: the matrix of weight of size (D + 1) x 10. Each column is the weight
-         vector of a Logistic Regression classifier.
-         X: the data matrix of size N x D
-
-     Output:
-         label: vector of size N x 1 representing the predicted label of
-         corresponding feature vector given in data matrix
-
-    """
-    label = np.zeros((data.shape[0], 1))
-
-    ##################
-    # YOUR CODE HERE #
-    ##################
-    # HINT: Do not forget to add the bias term to your input data
-
+    #print (" num samples ",data.shape[0])
+    Number_of_Samples = data.shape[0]
+    BiasTerm = np.ones((Number_of_Samples,1))
+    DataWithBias = np.concatenate((BiasTerm, data), axis = 1)
+    # print("datawithbias .. ",DataWithBias.shape[0],DataWithBias.shape[1])
+    EstimateLabels = np.zeros((Number_of_Samples,10))
+    " Find WT X"
+    EstimateLabels = sigmoid(np.dot(DataWithBias, W))
+    " Find the max of the all the possiblities"
+    label = np.zeros((Number_of_Samples, 1))
+    label = np.argmax(EstimateLabels,axis=1)
+    " Reshape it to a N*1 "
+    label = label.reshape(Number_of_Samples,1)
+    # print ("label reshape done ... ",label.shape[0],label.shape[1])
     return label
-
 
 """
 Script for Logistic Regression
@@ -246,6 +210,7 @@ for i in range(n_class):
 
 # Find the accuracy on Training Dataset
 predicted_label = blrPredict(W, train_data)
+print('\n Logistic Regression')
 print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
 
 # Find the accuracy on Validation Dataset
@@ -264,64 +229,55 @@ print('SVM\nLinear Kernel');
 train_label = np.squeeze(train_label)
 clf = SVC(kernel = 'linear')
 clf.fit(train_data, train_label)
-print("Training Accuracy:   ",clf.score(train_data, train_label))
-print("Test Accuracy:       ",clf.score(test_data, test_label))
-print("Validation Accuracy: ",clf.score(validation_data, validation_label))
+print("Training Accuracy:   "+str(100*clf.score(train_data, train_label))+"%")
+print("Test Accuracy:       "+str(100*clf.score(test_data, test_label))+"%")
+print("Validation Accuracy: "+str(100*clf.score(validation_data, validation_label))+"%")
 
 print ("\n\nGamma\n");
-clf = SVC(gamma = 1)
+clf = SVC(kernel='rbf',gamma = 1)
 clf.fit(train_data, train_label)
-print("Training Accuracy:   ",clf.score(train_data, train_label))
-print("Test Accuracy:       ", clf.score(test_data, test_label))
-print("Validation Accuracy: ",clf.score(validation_data, validation_label))
+print("Training Accuracy:   "+str(100*clf.score(train_data, train_label))+"%")
+print("Test Accuracy:       "+str(100*clf.score(test_data, test_label))+"%")
+print("Validation Accuracy: "+str(100*clf.score(validation_data, validation_label))+"%")
 
 print ("\n\nRBF Kernel \n");
 clf = SVC(kernel = 'rbf')
 clf.fit(train_data, train_label)
-print("Training Accuracy:   ",clf.score(train_data, train_label))
-print("Test Accuracy:       ", clf.score(test_data, test_label))
-print("Validation Accuracy: ",clf.score(validation_data, validation_label))
+print("Training Accuracy:   "+str(100*clf.score(train_data, train_label))+"%")
+print("Test Accuracy:       "+str(100*clf.score(test_data, test_label))+"%")
+print("Validation Accuracy: "+str(100*clf.score(validation_data, validation_label))+"%")
 
-##Code for plotting the graph of accuracy with respect to varying C values
-#
-#vector = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100] #C values stored in a vector
-#vectorTrain = []
-#vectorTest = []
-#vectorValidate = []
-#
-#for i in vector:
-#    train_label = np.squeeze(train_label)
-#    clf = SVC(C = i)
-#    print("Doing for C: ",i)
-#    clf.fit(train_data, train_label)
-#    print("Done fitting")
-#    accuracy_train = clf.score(train_data, train_label)
-#    print("accuracy of train: ",accuracy_train)
-#    accuracy_test = clf.score(test_data, test_label)
-#    print("accuracy of test: ",accuracy_test)
-#    accuracy_validation = clf.score(validation_data, validation_label)
-#    print("accuracy of validation: ",accuracy_validation)
-#    vectorTrain.append(accuracy_train)
-#    vectorTest.append(accuracy_test)
-#    vectorValidate.append(accuracy_validation)
-#    print("\n-------------------------------\n")
-#
-#
-#vectorTrain_new = [i * 100 for i in vectorTrain]  #Converted accuracy into percentage
-#vectorTest_new = [i * 100 for i in vectorTest]
-#vectorValidate_new = [i * 100 for i in vectorValidate]
-#
-#accuracyMatrix = np.column_stack((vectorTrain_new, vectorTest_new, vectorValidate_new))
-#    
-#fig = plt.figure(figsize=[12,6])
-#plt.subplot(1, 2, 1)
-#plt.plot(vector,accuracyMatrix)
-#plt.title('Accuracy with varying values of C')
-#plt.legend(('Testing data','Training data', 'Validation data'), loc = 'best') 
-#plt.xlabel('C values')
-#plt.ylabel('Accuracy in %') 
-#plt.show()
-#
+
+cvalues  = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+TrainingAccuracy = []
+TestAccuracy = []
+ValidationAccuracy = []
+print ("---------------------------------------------------")
+for i in cvalues:
+    train_label = np.squeeze(train_label)
+    clf = SVC(C=i,kernel='rbf')
+    print("Doing for C-Value: ",i)
+    clf.fit(train_data, train_label)
+    print("CLF Fitting Done!")
+    print("Training Accuracy:   ",100*clf.score(train_data, train_label),"%")
+    print("Testing Accuracy:    ",100*clf.score(test_data, test_label),"%")
+    print("Validation Accuracy: ",100*clf.score(validation_data, validation_label),"%")
+    TrainingAccuracy.append(100*clf.score(train_data, train_label))
+    TestAccuracy.append(100*clf.score(test_data, test_label))
+    ValidationAccuracy.append(100*clf.score(validation_data, validation_label))
+    print ("---------------------------------------------------")
+
+accuracyMatrix = np.column_stack((TrainingAccuracy, TestAccuracy, ValidationAccuracy))
+
+fig = plt.figure(figsize=[12,6])
+plt.subplot(1, 2, 1)
+plt.plot(vector,accuracyMatrix)
+plt.title('Accuracy with varying values of C')
+plt.legend(('Testing data','Training data', 'Validation data'), loc = 'best')
+plt.xlabel('C values')
+plt.ylabel('Accuracy in %')
+plt.show()
+
 
 
 """
@@ -330,12 +286,13 @@ Script for Extra Credit Part
 # FOR EXTRA CREDIT ONLY
 W_b = np.zeros((n_feature + 1, n_class))
 initialWeights_b = np.zeros((n_feature + 1, n_class))
-opts_b = {'maxiter': 1}
+opts_b = {'maxiter': 100}
 
 args_b = (train_data, Y)
 nn_params = minimize(mlrObjFunction, initialWeights_b, jac=True, args=args_b, method='CG', options=opts_b)
 W_b = nn_params.x.reshape((n_feature + 1, n_class))
 
+print('\n Multiclass logistic Regression');
 # Find the accuracy on Training Dataset
 predicted_label_b = mlrPredict(W_b, train_data)
 print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label_b == train_label).astype(float))) + '%')
